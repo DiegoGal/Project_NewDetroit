130a897449700827c28c2053651c231cc437873e
Guillermo <thecrow88@gmail.com>
2014-01-14 17:33:02 +0100

    We check the collision against fog plane every second, instead of all time.

--- New Detroit Project/Assets/Scripts/FogOfWar/FogOfWarUnit.cs
+++ New Detroit Project/Assets/Scripts/FogOfWar/FogOfWarUnit.cs
@@ -7,7 +7,8 @@ public class FogOfWarUnit : MonoBehaviour {
 	//=====     Attributes     =====
 	//==============================
 	private float radius;	//radius vision.
-
+	private float time;		//time to hit the fog plane
+	private List<Vector3> positions = new List<Vector3> ();		//point to show the position in fog plane
 
 
 	//=====================================
@@ -22,22 +23,41 @@ public class FogOfWarUnit : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		radius = 2.0f;
+		time = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//update the collision with the fog plane.
-		RaycastHit[] hits;
-		hits = Physics.RaycastAll (transform.position, Vector3.up);
-		for (int i = 0; i < hits.Length; i ++)
+		// Accumulated time since last update of collision points.
+		time += Time.deltaTime;
+		// Check the collision every second.
+		if (time >= 1) 
 		{
-			RaycastHit hit = hits[i];
-			MeshFilter filter = hit.collider.GetComponent<MeshFilter>();
-			Vector3 relativePoint;
-			if (filter)
+			// Reset time and points for updating them.
+			time = 0;
+			positions.Clear ();
+			// Update the collision with the fog plane.
+			RaycastHit[] hits;
+			hits = Physics.RaycastAll (transform.position, Vector3.up);
+			for (int i = 0; i < hits.Length; i ++)
 			{
-				relativePoint = filter.transform.InverseTransformPoint(hit.point);
-				FogOfWarPlane.positions.Add (relativePoint);
+				RaycastHit hit = hits[i];
+				MeshFilter filter = hit.collider.GetComponent<MeshFilter>();
+				Vector3 relativePoint;
+				if (filter)
+				{
+					relativePoint = filter.transform.InverseTransformPoint(hit.point);
+					FogOfWarPlane.positions.Add (relativePoint);
+					// Save the collision points
+					positions.Add(relativePoint);
+				}
+			}
+		}
+		else
+		{
+			for (int i = 0; i < positions.Count; i ++)
+			{
+				FogOfWarPlane.positions.Add (positions[i]);
 			}
 		}
 	}
