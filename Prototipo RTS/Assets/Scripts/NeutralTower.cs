b9298a7bad3057e637ae03d41c68f0c8d8ae916a
fermonas <fermonast@gmail.com>
2014-01-22 10:53:43 +0100

    The initialization of the Tower Neutral

---
+++ Prototipo RTS/Assets/Scripts/NeutralTower.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeutralTower : MonoBehaviour {
+
+	private enum TowerState
+	{
+		Neutral,
+		Iddle,
+		Searching, // espera hasta que halla hueco en la mina
+		ShootingEnemies
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void EnemyEntersInVisionSphere (UnitController enemy)
+	{
+		Debug.Log("TOWER SEARCHING");
+	}
+}
