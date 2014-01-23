b58c3dff3e1b520757fead365c1f30e49434b1c1
fermonas <fermonast@gmail.com>
2014-01-23 16:45:40 +0100

    More Tower

--- Prototipo RTS/Assets/Scripts/NeutralTower.cs
+++ Prototipo RTS/Assets/Scripts/NeutralTower.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NeutralTower : MonoBehaviour {
+
+	public int teamNumber;
 
 	private enum TowerState
 	{
@@ -11,6 +14,20 @@ public class NeutralTower : MonoBehaviour {
 		ShootingEnemies
 	}
 
+	private TowerState currentTowerState = TowerState.Iddle;
+
+	//Conts for Tower conquest
+	private float contConq1 = 0.0f;
+	private float contConq2 = 0.0f;
+
+	//Constant when the tower is conquered
+	private const float finalCont = 100.0f;
+
+	//The life of the Tower
+	private float life = 100.0f;
+
+	private List<GameObject> unitsSearched = new List<GameObject>();
+
 	// Use this for initialization
 	void Start () {
 		
@@ -18,11 +35,41 @@ public class NeutralTower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
+		switch (currentTowerState) 
+		{
+			case TowerState.Neutral:
+					
+
+				break;
+
+			case TowerState.Iddle:
+				
+
+				break;
+
+			case TowerState.Searching:
+			
+				break;
+
+			case TowerState.ShootingEnemies:
+			break;
+				
+		}
 	}
 	
 	public void EnemyEntersInVisionSphere (UnitController enemy)
 	{
-		Debug.Log("TOWER SEARCHING");
+
+		if (currentTowerState == TowerState.Iddle) 
+		{
+			currentTowerState = TowerState.Searching;
+			//unitsSearched.Add(enemy);
+			Debug.Log ("Enemy entered in TOWER");
+		}
+		else
+		{
+		
+		}
 	}
 }
