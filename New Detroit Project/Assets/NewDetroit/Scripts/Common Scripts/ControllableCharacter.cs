b8d3c45d79d6bf2fc4ef6bc7adb33427eee5de80
maxi-jp <juventudperdia@gmail.com>
2014-02-19 16:58:34 +0100

    New class added: ControllableCharacter

---
+++ New Detroit Project/Assets/NewDetroit/Scripts/Common Scripts/ControllableCharacter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllableCharacter : MonoBehaviour
+{
+    // number that identifies the team to which the character belongs
+    public int teamNumber;
+
+    // Life variables
+    public float maximunLife = 100.0f;
+    protected float currentLife;
+
+    // Reference to the position of the unit team base
+    protected Vector3 basePosition = new Vector3();
+    // Reference to the base
+    public BaseController baseController;
+
+    public void SetBasePosition (Vector3 basePosition)
+    {
+        this.basePosition = basePosition;
+    }
+
+    public void SetArmyBase (BaseController baseController)
+    {
+        this.baseController = baseController;
+    }
+
+    public virtual bool Damage (float damage)
+    {
+        //Debug.Log("damage");
+        currentLife -= damage;
+        return (currentLife <= 0);
+    }
+
+} // class ControllableCharacter
