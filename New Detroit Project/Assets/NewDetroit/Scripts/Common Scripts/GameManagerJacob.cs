ed10b82f9d46a892f376557d5294715cdc06867a
DiegoGallu <diego.davilaguilar@gmail.com>
2014-05-31 19:47:51 +0200

    Changes for the menu

---
+++ New Detroit Project/Assets/NewDetroit/Scripts/Common Scripts/GameManagerJacob.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerJacob : MonoBehaviour {
+
+
+    public Vector3 cameraPositionRTS;
+    public Vector3 cameraRotationRTS;
+    public bool onGUI = true;
+    public GameObject menu;
+    public GameObject RobRender;
+
+	// Use this for initialization
+	void Start () {
+        Camera.mainCamera.transform.position = new Vector3(75.37802f, 10.55257f, -106.6093f);
+        Camera.mainCamera.transform.rotation = new Quaternion(27f, 0f, 0f,180f);
+	}
+
+    public void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 40, 120, 40), "", GUIStyle.none))
+        {
+            Destroy(menu);
+            GameObject instRob = (GameObject)Instantiate(RobRender, new Vector3(95f,3f,-116f),Quaternion.identity);
+            // Activar cosas de rob
+            instRob.GetComponent<ThirdPersonCamera>().enabled = true;
+            instRob.GetComponent<ThirdPersonNetwork>().enabled = false;
+        }
+
+        //GUILayout.EndHorizontal();
+
+        if (GUI.Button(new Rect(0, 90, 120, 40), "", GUIStyle.none))
+        {
+            Destroy(menu);
+        }
+
+        if (GUI.Button(new Rect(Screen.width - 120, 180, 120, 40), "", GUIStyle.none))
+        {
+            Application.Quit();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
