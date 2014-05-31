829cc5be7faec26ea5799d0487fa4eff61b36357
maxi-jp <juventudperdia@gmail.com>
2014-05-31 19:37:49 +0200

    Avances

--- New Detroit Project/Assets/NewDetroit/Scripts/RTS Scripts/UnitEngineer.cs
+++ New Detroit Project/Assets/NewDetroit/Scripts/RTS Scripts/UnitEngineer.cs
@@ -39,11 +39,11 @@ public class UnitEngineer : UnitController
     public GameObject towerArmyPrefab;
     public GameObject warehousePrefab;
     // The items towerArmy and warehouse
-    private GameObject towerArmy;
-    private GameObject warehouse;
+    protected GameObject towerArmy;
+    protected GameObject warehouse;
 
-    private bool newTGConstruct = false;
-    private bool newWConstruct = false;
+    protected bool newTGConstruct = false;
+    protected bool newWConstruct = false;
     private Vector3 constructDestiny = new Vector3();
 
     // dummy where is going to be instanciated a laptop
@@ -55,8 +55,8 @@ public class UnitEngineer : UnitController
     public GameObject hammer;
 
     // instances of the laptop and the hammer
-    private GameObject laptopInst;
-    private GameObject hammerInst;
+    protected GameObject laptopInst;
+    protected GameObject hammerInst;
 
     //For attacking1
     public GameObject fireball;
@@ -68,44 +68,6 @@ public class UnitEngineer : UnitController
 
     // To knows the direction of the fireball
     public Vector3 fireballDir;
-
-    public override void Awake ()
-    {
-        base.Awake();
-
-        // Por si no se han establecido las referencias a los dummys del modelo
-        // en el editor de Unity las buscamos ahora:
-        if (dummyLaptop == null)
-            dummyLaptop = transform.FindChild("Bip001/Bip001 Footsteps");
-        if (dummyHand == null)
-            dummyHand = transform.FindChild("Bip001/Bip001 Pelvis/Bip001 Spine/Bip001 Spine1/Bip001 Neck/Bip001 R Clavicle/Bip001 R UpperArm/Bip001 R Forearm/Bip001 R Hand/Mano Der");
-
-        // instanciate a laptop
-        laptopInst = Instantiate
-        (
-            laptop,
-            dummyLaptop.transform.position,
-            new Quaternion()
-        ) as GameObject;
-        laptopInst.transform.name = "Laptop";
-        laptopInst.transform.parent = dummyLaptop;
-        laptopInst.transform.rotation = transform.rotation;
-        // hide it
-        laptopInst.SetActive(false);
-
-        // instanciate a Hammer
-        hammerInst = Instantiate
-        (
-            hammer,
-            dummyHand.transform.position,
-            new Quaternion()
-        ) as GameObject;
-        hammerInst.transform.name = "Hammer";
-        hammerInst.transform.parent = dummyHand;
-        hammerInst.transform.Rotate(new Vector3(90.0f, 0.0f, 0.0f));
-        // hide it
-        hammerInst.SetActive(false);
-    }
 
     public override void Start ()
     {
@@ -1255,37 +1217,9 @@ public class UnitEngineer : UnitController
 		return newTGConstruct || newWConstruct;
 	}
 
-    public void SetCanConstruct (int item)
+    public virtual void SetCanConstruct (int item)
 	{
-        switch (item)
-        {
-            case 0:
-                towerArmy = Instantiate
-                (
-                    towerArmyPrefab,
-                    transform.position + new Vector3(30,-30,30),
-                    new Quaternion(0, 0, 0, 0)
-                ) as GameObject;
-                towerArmy.transform.Rotate(270.0f, 0.0f, 0.0f);
-                towerArmy.name = towerArmy.name.Replace("(Clone)", "");
-                towerArmy.GetComponent<TowerArmy>().SetTeamNumber(this.teamNumber, team.teamColorIndex);
-                towerArmy.GetComponent<TowerArmy>().SetBaseController(baseController);
-                newTGConstruct = true;
-                break;
-            case 1:
-                warehouse = Instantiate
-                (
-                    warehousePrefab,
-                    transform.position + new Vector3(30, -30, 30),
-                    new Quaternion(0, 0, 0, 0)
-                ) as GameObject;
-                warehouse.transform.Rotate(0.0f, 180.0f, 0.0f);
-                warehouse.name = warehouse.name.Replace("(Clone)", "");
-                warehouse.GetComponent<Warehouse>().SetTeamNumber(this.teamNumber, team.teamColorIndex);
-                warehouse.GetComponent<Warehouse>().SetBaseController(baseController);
-                newWConstruct = true;
-                break;
-        }
+        
 	}
 
     public override int GetUnitType ()
