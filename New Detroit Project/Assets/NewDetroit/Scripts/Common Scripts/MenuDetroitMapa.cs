fa4a66f38715b3f57e7fe1262b5e023a87bd795f
fermonas <fermonast@gmail.com>
2014-06-12 11:56:06 +0200

    New scene for Guille and Fernando.

--- New Detroit Project/Assets/NewDetroit/Scripts/Common Scripts/MenuDetroitMapa.cs
+++ New Detroit Project/Assets/NewDetroit/Scripts/Common Scripts/MenuDetroitMapa.cs
@@ -12,7 +12,7 @@ public class MenuDetroitMapa : MonoBehaviour
 
     public static readonly string SceneNameMenu = "MenuDetroitMapa";
 
-    public static readonly string SceneNameGame = "RTS_Units_Test_Maxi_Online";
+    public static readonly string SceneNameGame = Application.loadedLevelName; // "RTS_Units_Test_Maxi_Online";
 
     public void Awake()
     {
