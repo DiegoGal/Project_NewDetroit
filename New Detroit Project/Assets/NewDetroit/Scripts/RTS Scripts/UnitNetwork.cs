62990c6e1aa93a97f66c50c3f433b09038d63208
Guillermo Cuesta <thecrow88@gmail.com>
2014-06-13 20:39:24 +0200

    I have cleaned UnitNetwork script for RTS online and I have moved UnitNetworkHarvester script to deprecated code.

--- New Detroit Project/Assets/NewDetroit/Scripts/RTS Scripts/UnitNetwork.cs
+++ New Detroit Project/Assets/NewDetroit/Scripts/RTS Scripts/UnitNetwork.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class UnitNetwork : Photon.MonoBehaviour
+public class UnitNetwork : BasicNetwork
 {
 
 	private CSelectable selectableScript;
@@ -11,19 +11,13 @@ public class UnitNetwork : Photon.MonoBehaviour
 	private FogOfWarUnit fogOfWarScript;
 	private NavMeshAgent navMeshAgent;
 
-    private Vector3 correctPlayerPos = Vector3.zero; // We lerp towards this
-    private Quaternion correctPlayerRot = Quaternion.identity; // We lerp towards this
-    //private UnitBasicArtillery.ArtilleryState state; // new State of the ArtilleryState
-    //private UnitController.State unitState; // new State of Unit
-    protected UnitController.State lastState = UnitController.State.Idle; // last State of Unit
-    //private float currentLife; // for damage
-    //private bool attack2Selected; // to see if the attack has changed
-
     // modelo del asset (el que contiene las animaciones)
     protected Transform model;
 	
-	void Awake ()
+	public virtual void Awake ()
 	{
+		base.Awake();
+	
 		selectableScript      = GetComponent<CSelectable>();
         unitScript            = GetComponent<UnitController>();
 		fogOfWarScript	      = GetComponent<FogOfWarUnit>();
@@ -52,9 +46,6 @@ public class UnitNetwork : Photon.MonoBehaviour
 
             //Debug.Log("not MINE");
 		}
-		
-		gameObject.name = gameObject.name + "_" + photonView.viewID;
-        gameObject.name = gameObject.name.Replace("(Clone)", ""); 
 
         // se captura la referencia al modelo
         model = transform.FindChild("Model");
@@ -62,17 +53,10 @@ public class UnitNetwork : Photon.MonoBehaviour
 	
 	public virtual void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
 	{
+		base.OnPhotonSerializeView(stream, info);
+		
 		if (stream.isWriting)
 		{
-			//We own this player: send the others our data
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-
-            // TODO! ñapa de pm, hay que hacerlo bien
-            //stream.SendNext(GetComponent<CTeam>().teamNumber);
-
-            stream.SendNext(stateScript.currentState);
-
             stream.SendNext(stateScript.animationSend);
             stream.SendNext(stateScript.animationName);
             stream.SendNext(stateScript.animationSendQeued);
@@ -80,75 +64,13 @@ public class UnitNetwork : Photon.MonoBehaviour
 
 			if (stateScript.animationSend) stateScript.animationSend = false;
 			if (stateScript.animationSendQeued) stateScript.animationSendQeued = false;
-
-            //UnitBasicArtillery script = this.GetComponent<UnitBasicArtillery>();
-            //stream.SendNext(script.currentArtilleryState);
-            //stream.SendNext(script.currentState);
-            //stream.SendNext(script.lastState);
-            //stream.SendNext(script.getLife());
-            //stream.SendNext(script.attack2Selected);
 		}
 		else
-		{
-			//Network player, receive data
-			correctPlayerPos = (Vector3)stream.ReceiveNext();
-			correctPlayerRot = (Quaternion)stream.ReceiveNext();
-
-            // TODO! ñapa de pm, hay que hacerlo bien
-            //GetComponent<CTeam>().teamNumber = (int)stream.ReceiveNext();
-
-            stateScript.currentState = (UnitController.State)stream.ReceiveNext();
-
+		{            
             stateScript.animationChanged = (bool)stream.ReceiveNext();
             stateScript.animationName = (string)stream.ReceiveNext();
             stateScript.animationChangeQueued = (bool)stream.ReceiveNext();
             stateScript.animationNameQueued = (string)stream.ReceiveNext();
-
-            // comprobar el cambio
-
-            //state =            (UnitBasicArtillery.ArtilleryState)stream.ReceiveNext();
-            //unitState =        (UnitController.State)stream.ReceiveNext();
-            //lastState =        (UnitController.State)stream.ReceiveNext();
-            //currentLife =      (float)stream.ReceiveNext();
-            //attack2Selected  = (bool)stream.ReceiveNext();
 		}
 	}
-	
-	public virtual void Update ()
-	{
-		if (!photonView.isMine)
-		{
-			//Update remote player (smooth this, this looks good, at the cost of some accuracy)
-			transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
-			transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
-
-            /*if (lastState != stateScript.currentState)
-            {
-                switch (stateScript.currentState)
-                {
-                    case UnitController.State.Idle: animation.CrossFade("Idle01"); break;
-                    case UnitController.State.GoingTo: animation.CrossFade("Walk"); break;
-                    case UnitController.State.GoingToAnEnemy: animation.CrossFade("Walk"); break;
-                    case UnitController.State.Attacking: animation.CrossFade("Attack1"); break;
-                    //case UnitController.State.Dying:          animation.CrossFade("Die");     break;
-                    case UnitController.State.Flying: break;
-                    case UnitController.State.AscendingToHeaven:
-                        // TODO! cambiar el material de la unidad
-
-
-                        break;
-                }
-            }*/
-            lastState = stateScript.currentState;
-
-            //UnitBasicArtilleryRemote script = GetComponent<UnitBasicArtilleryRemote>();
-            //script.currentArtilleryState = state;
-            //script.lastState = lastState;
-            //script.currentState = unitState;
-            //script.attack2Selected = attack2Selected;
-		}
-	}
-	
-
-
 }
