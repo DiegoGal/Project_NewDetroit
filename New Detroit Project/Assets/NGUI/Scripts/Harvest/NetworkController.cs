2bf06e0369c5b4d266ecbcda6646b3a6fba11f93
DiegoGallu <diego.davilaguilar@gmail.com>
2014-06-16 18:44:19 +0200

    Character selection, added the selection menu with the prefabs.

--- New Detroit Project/Assets/NGUI/Scripts/Harvest/NetworkController.cs
+++ New Detroit Project/Assets/NGUI/Scripts/Harvest/NetworkController.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkController : Photon.MonoBehaviour {
+public class NetworkController : Photon.MonoBehaviour
+{
 
-	//private string roomName = "myRoom";
+    //private string roomName = "myRoom";
 
     private Vector2 scrollPos = Vector2.zero;
 
@@ -25,7 +26,6 @@ public class NetworkController : Photon.MonoBehaviour {
     private short selected;
     private string roomName;
     public UILabel info;
-    public UIWidgetContainer wgContainer;
 
     public void Start()
     {
@@ -67,17 +67,31 @@ public class NetworkController : Photon.MonoBehaviour {
     public void CreateRoom()
     {
         UILabel roomName = labelRoomName.GetComponent<UILabel>();
-        if (roomName.text.Equals(""))
+        RoomInfo[] roomsInfo = PhotonNetwork.GetRoomList();
+        int i = 0; bool enc = false;
+        while (i < roomsInfo.Length && !enc)
         {
-            PhotonNetwork.CreateRoom("Room" + Random.Range(1,9999), true, true, 4);
-            info.text = "Room created.";
+            if (roomsInfo[i].name.Equals(roomName))
+                enc = true;
+        }
+        // If there's no other roome with the same name
+        if (!enc)
+        {
+            if (roomName.text.Equals(""))
+            {
+                PhotonNetwork.CreateRoom("Room" + Random.Range(1, 9999), true, true, 4);
+                info.text = "Room created.";
+            }
+            else
+            {
+                PhotonNetwork.CreateRoom(roomName.text, true, true, 4);
+                info.text = "Room " + roomName.text + " created.";
+            }
         }
         else
         {
-            PhotonNetwork.CreateRoom(roomName.text, true, true, 4);
-            info.text = "Room " + roomName.text + " created.";
+            info.text = "already exist a room with that name";
         }
-        
     }
 
     public void JoinRoom()
@@ -89,7 +103,7 @@ public class NetworkController : Photon.MonoBehaviour {
         else
         {
             RoomInfo[] roomsInfo = PhotonNetwork.GetRoomList();
-            
+
             if (roomsInfo[selected].playerCount == roomsInfo[selected].maxPlayers)
             {
                 info.text = "This room is full";
@@ -111,7 +125,7 @@ public class NetworkController : Photon.MonoBehaviour {
         info.text = roomsInfo.Length + " rooms";
         UILabel roomScript = labelRooms.GetComponent<UILabel>();
         UILabel playerScript = labelPlayers.GetComponent<UILabel>();
-        roomScript.text = playerScript .text = "";
+        roomScript.text = playerScript.text = "";
         originalButton.SetActive(true);
         originalButton.transform.GetChild(0).gameObject.SetActive(true);
         for (int i = 0; i < layerButons.Count; i++)
@@ -127,10 +141,10 @@ public class NetworkController : Photon.MonoBehaviour {
             button.transform.position = originalButton.transform.position;
             button.transform.localScale = originalButton.transform.localScale;
 
-            button.transform.localPosition += new Vector3(0,-12*i,0); 
+            button.transform.localPosition += new Vector3(0, -12 * i, 0);
             layerButons.Add(button);
 
-            roomScript.text =roomScript.text +  roomsInfo[i].name + "\n";
+            roomScript.text = roomScript.text + roomsInfo[i].name + "\n";
             playerScript.text = playerScript.text + roomsInfo[i].playerCount + "/" + roomsInfo[i].maxPlayers + "\n";
         }
         originalButton.SetActive(false);
@@ -159,12 +173,15 @@ public class NetworkController : Photon.MonoBehaviour {
         }
         else
         {
-            selected = sel;            
+            selected = sel;
             RoomInfo[] roomsInfo = PhotonNetwork.GetRoomList();
             roomName = roomsInfo[sel].name;
             button.defaultColor = new Color(button.defaultColor.r, button.defaultColor.g, button.defaultColor.b, 0.5f);
         }
-        info.text = selected + " selected";
+        if (roomName.Equals(""))
+            info.text = "none selected";
+        else
+            info.text = roomName + " selected";
     }
 
     // We have two options here: we either joined(by title, list or random) or created a room.
@@ -176,7 +193,7 @@ public class NetworkController : Photon.MonoBehaviour {
     public void OnCreatedRoom()
     {
         Debug.Log("OnCreatedRoom");
-        PhotonNetwork.LoadLevel(SceneNameGame);
+        //PhotonNetwork.LoadLevel(SceneNameGame);
     }
 
 
---
+++ New Detroit Project/Assets/NewDetroit/Scripts/Common Scripts/AnimationHeroMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationHeroMenu : MonoBehaviour {
+
+    public GameObject army;
+    public GameObject heroe;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnHover(bool isOver)
+    {
+        if (heroe.activeInHierarchy)
+        {
+            if (heroe.name.Equals("Skelterbot"))
+            {
+                if (isOver)
+                    heroe.animation.CrossFade("Run");
+                else
+                    heroe.animation.CrossFade("Idle01");
+            }
+            else
+            {
+                if (isOver)
+                    heroe.animation.CrossFade("Run");
+                else
+                    heroe.animation.CrossFade("Iddle01");
+            }
+        }
+        if (army.activeInHierarchy)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (isOver)
+                    army.transform.GetChild(i).gameObject.animation.CrossFade("Walk");
+                else 
+                    army.transform.GetChild(i).gameObject.animation.CrossFade("Idle01");
+            }
+        }
+    }
+}
